8adda1c4fa7fa91809e696210b0885fe7b9bc3cd
unknown <nur.mnzaman@gmail.com>
2023-11-20 23:58:51 +0600

    []

--- Firebase Authentication/Firebase-Authentication-Email-Password-in-React/src/App.jsx
+++ Firebase Authentication/Firebase-Authentication-Email-Password-in-React/src/App.jsx
@@ -4,19 +4,30 @@ import Signup from "./pages/Signup";
 import { Login } from "./pages/Login";
 import { Protected } from "./components/Protected";
 import { Home } from "./pages/Home";
+import RouterOutlet from "./components/RouterOutlet";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<RouterOutlet />}>
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            <Route path="/" element={<Protected />}>
+              <Route path="/" index element={<Home />} />
+            </Route>
+          </Route>
+        </Routes>
+
+        {/* <Routes>
           <Route path="/" />
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={<Login />} />
           <Route path="/" element={<Protected />}>
             <Route path="/" index element={<Home />} />
           </Route>
-        </Routes>
+        </Routes> */}
       </BrowserRouter>
     </>
   );
---
+++ Firebase Authentication/Firebase-Authentication-Email-Password-in-React/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiurkOAU9CTdTPyv40-7fCAILFSK0JOZA",
+  authDomain: "fir-auth-email-pass-react.firebaseapp.com",
+  projectId: "fir-auth-email-pass-react",
+  storageBucket: "fir-auth-email-pass-react.appspot.com",
+  messagingSenderId: "132186366027",
+  appId: "1:132186366027:web:1758dbfd5a5a45f448edce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
